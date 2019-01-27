28a5806fe4d230d717bab0db7e24604a12cd08ff
herdanu yudistira putra <heh.danu@gmail.com>
2019-01-27 09:08:41 +0700

    Updates

---
+++ precache-manifest.b6f5e0786cbed945e94830317c25ea6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77ec93ea04e44834cb15",
+    "url": "/simple-shopping-cart/static/js/runtime~main.77ec93ea.js"
+  },
+  {
+    "revision": "ac9c716fb2523b3b06b7",
+    "url": "/simple-shopping-cart/static/js/main.ac9c716f.chunk.js"
+  },
+  {
+    "revision": "dbc367c0abb8d545dde7",
+    "url": "/simple-shopping-cart/static/js/1.dbc367c0.chunk.js"
+  },
+  {
+    "revision": "ac9c716fb2523b3b06b7",
+    "url": "/simple-shopping-cart/static/css/main.4b768c13.chunk.css"
+  },
+  {
+    "revision": "dbc367c0abb8d545dde7",
+    "url": "/simple-shopping-cart/static/css/1.8095ef58.chunk.css"
+  },
+  {
+    "revision": "0fb48482a33e91ebe6f252d9f59e23ea",
+    "url": "/simple-shopping-cart/index.html"
+  }
+];
