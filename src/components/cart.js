997f39848921173a2741eca422da036eb35676a1
herdanu yudistira putra <heh.danu@gmail.com>
2019-01-27 09:41:49 +0700

    add cart button css

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,3 +1,4 @@
+//@flow
 import React, { Fragment } from "react";
 import { Table } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/components/products.js
+++ src/components/products.js
@@ -8,13 +8,13 @@ const Products = ({ product, addToCart }) => {
 
       <div className="product-info">
         <Row>
-          <Col md={6}>
+          <Col md={4}>
             <div className={`group-title-and-price`}>
               <div className="title">product {product.id}</div>
               <div className="price">${product.price}</div>
             </div>
           </Col>
-          <Col md={6} className="centering-add-to-cart">
+          <Col md={8} className="centering-add-to-cart">
             <div onClick={() => addToCart(product)} className="add-to-cart-button">add to cart</div>
           </Col>
         </Row>
